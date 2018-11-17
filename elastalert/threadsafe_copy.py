d4c58847a79fde44a397b6a11f94c6f209ac26d3
Joey Andres <joey@jsonar.com>
2018-11-16 16:21:40 -0800

    Fix multithreading with copy.

--- elastalert/threadsafe_copy.py
+++ elastalert/threadsafe_copy.py
@@ -3,18 +3,19 @@ import threading
 
 
 class ThreadsafeCopy:
+    copy_lock = threading.Lock()
+    deepcopy_lock = threading.Lock()
+
     """
     copy methods are throwing thread safe exceptions since
     """
     @staticmethod
     def copy(obj):
-        lock = threading.Lock()
-        with lock:
+        with ThreadsafeCopy.copy_lock:
             return copy.copy(obj)
 
     @staticmethod
     def deepcopy(obj):
-        lock = threading.Lock()
-        with lock:
+        with ThreadsafeCopy.deepcopy_lock:
             return copy.deepcopy(obj)
 
